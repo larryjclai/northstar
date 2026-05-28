e0653cf72dc1c36d165387f2d194197f165030ca
賴瑞晟 <skylovely2005@hotmail.com>
2026-05-28 23:52:01 +0800

    chore: add test-chart scratch file

---
+++ test-chart.ts
@@ -0,0 +1,4 @@
+import { readFileSync } from 'fs';
+import Database from 'better-sqlite3';
+
+const db = new Database('/Users/juicheng/Library/Application Support/com.northstar.dev/northstar.db'); // Wait, I don't know the DB path.
